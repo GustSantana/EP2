63266ed6f8e8bb8b234517cac8c6a8bc9692b0f9
gustsantana <gustavo.sant2910@gmail.com>
2022-12-04 18:14:30 -0300

    .

--- GerenciadorDeMemoria.cpp
+++ GerenciadorDeMemoria.cpp
@@ -17,8 +17,6 @@ void GerenciadorDeMemoria::load(string arquivo, MemoriaRAM* m){
     int tamanho, valor, valor2, valor3, posicao = 0;
     string tipo;
 
-    Instrucao* instrucao;
-
     ler >> tamanho;
     ler >> tipo >> valor;
     while(ler){
@@ -29,47 +27,47 @@ void GerenciadorDeMemoria::load(string arquivo, MemoriaRAM* m){
         else{
             if(tipo == "LW"){
                 ler >> valor2;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarLW(valor, valor2));
-                return;
             }
             if(tipo == "SW"){
                 ler >> valor2;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarSW(valor, valor2));
-                return;
             }
             if(tipo == "J"){
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarJ(valor));
-                return;
             }
             if(tipo == "BNE"){
                 ler >> valor2 >> valor3;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarBNE(valor, valor2, valor3));
-                return;
             }
             if(tipo == "BEQ"){
                 ler >> valor2 >> valor3;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarBEQ(valor, valor2, valor3));
-                return;
             }
             if(tipo == "ADD"){
                 ler >> valor2 >> valor3;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarADD(valor, valor2, valor3));
-                return;
             }
             if(tipo =="SUB"){
                 ler >> valor2 >> valor3;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarSUB(valor, valor2, valor3));
-                return;
             }
             if(tipo == "MULT"){
                 ler >> valor2;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarMULT(valor, valor2));
-                return;
             }
             if(tipo == "DIV"){
                 ler >> valor2;
+                Instrucao* instrucao;
                 m->escrever(posicao, instrucao->criarDIV(valor, valor2));
-                return;
             }
         }
         posicao++;
@@ -98,57 +96,38 @@ void GerenciadorDeMemoria::dump(string arquivo, MemoriaRAM* m){
         else{
             Instrucao *instrucao = dynamic_cast<Instrucao*>(m->ler(i));
 
-            if(instrucao->getOpcode() == Instrucao::LW){
+            if(instrucao->getOpcode() == Instrucao::LW)
                 escrever << "LW " << instrucao->getDestino() << " " << instrucao->getImediato() << endl;
-                return;
-            }
-            if(instrucao->getOpcode() == Instrucao::SW){
+
+            if(instrucao->getOpcode() == Instrucao::SW)
                 escrever << "SW " << instrucao->getDestino() << " " << instrucao->getImediato() <<  endl;
-                return;
-            }
 
-            if(instrucao->getOpcode() == Instrucao::J){
+            if(instrucao->getOpcode() == Instrucao::J)
                 escrever << "J " << instrucao->getImediato() << endl;
-                return;
-            }
-
-            if(instrucao->getOpcode() == Instrucao::BNE){
+            
+            if(instrucao->getOpcode() == Instrucao::BNE)
                 escrever << "BNE " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << " " << instrucao->getImediato() << endl;
-                return;
-            }
-
-            if(instrucao->getOpcode() == Instrucao::BEQ){
+            
+            if(instrucao->getOpcode() == Instrucao::BEQ)
                 escrever << "BEQ " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << " " << instrucao->getImediato() << endl;
-                return;
-            }
-
+            
             if(instrucao->getOpcode() == Instrucao::TIPO_R){
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_ADD){
+                if(instrucao->getFuncao() == Instrucao::FUNCAO_ADD)
                     escrever << "ADD " << instrucao->getDestino() << " " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
-                    return;
-                }
-
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_SUB){
+                
+                if(instrucao->getFuncao() == Instrucao::FUNCAO_SUB)
                     escrever << "SUB " << instrucao->getDestino() << " " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
-                    return;
-                }
-
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_MULT){
+                
+                if(instrucao->getFuncao() == Instrucao::FUNCAO_MULT)
                     escrever << "MULT " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
-                    return;
-                }
 
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_DIV){
-                   escrever << "DIV " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
-                    return; 
-                }
+                if(instrucao->getFuncao() == Instrucao::FUNCAO_DIV)
+                    escrever << "DIV " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
             }
-
             if(escrever.fail())
                 throw new runtime_error ("runtime_error");
-
+            
             int verificaOP = instrucao->getOpcode();
-
             if((verificaOP != Instrucao::TIPO_R) && (verificaOP != Instrucao::LW) && (verificaOP != Instrucao::SW) && 
             (verificaOP != Instrucao::BNE) && (verificaOP != Instrucao::BEQ) && (verificaOP != Instrucao::J) && (instrucao != NULL))
                 throw new runtime_error ("runtime_error");
