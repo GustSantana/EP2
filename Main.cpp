8cee92a8089458e9793035535e2764ff69a7b946
gustsantana <gustavo.sant2910@gmail.com>
2022-12-03 21:48:23 -0300

    Verifica  a opção 5 e 6, tem um erro q n sei res

--- Main.cpp
+++ Main.cpp
@@ -13,18 +13,18 @@ int main(){
 
 BancoDeRegistradores *registradores = new BancoDeRegistradores();
 MemoriaRAM *memoriaRam = new MemoriaRAM(64);
-ESMapeadaNaMemoria *esmapeadanamemoria = new ESMapeadaNaMemoria(memoriaRam);
+ESMapeadaNaMemoria *mapmemoria = new ESMapeadaNaMemoria(memoriaRam);
 Teclado *teclado = new Teclado;
 TecladoDeChar *tecladodechar = new TecladoDeChar;
 Monitor *monitor = new Monitor;
 MonitorDeChar *monitordechar = new MonitorDeChar;
-esmapeadanamemoria->adicionar(teclado);
-esmapeadanamemoria->adicionar(tecladodechar);
-esmapeadanamemoria->adicionar(monitor);
-esmapeadanamemoria->adicionar(monitordechar);
-UnidadeDeControle *unidadecontrole = new UnidadeDeControle(registradores, esmapeadanamemoria);
 
+mapmemoria->adicionar(teclado);
+mapmemoria->adicionar(tecladodechar);
+mapmemoria->adicionar(monitor);
+mapmemoria->adicionar(monitordechar);
 
+UnidadeDeControle *controle = new UnidadeDeControle(registradores, mapmemoria);
 
 Dado* dadoMain;
 
@@ -32,32 +32,12 @@ int escolha = -1;
 int escolhido;
 int escolhaMemoria;
 int escolhaRegistradores;
-int dadoPosicao;
-int dadoValor;
-int registradorPosicao;
-int registradorValor;
-
-// Instrucao* instrucao1 = new Instrucao(LW, -1, -1, 8, 0, -1);
-// Instrucao* instrucao2 = new Instrucao(LW, -1, -1, 9, 1, -1);
-// Instrucao* instrucao3 = new Instrucao(LW, -1, -1, 10, 2, -1);
-// Instrucao* instrucao4 = new Instrucao(BNE, 9, 10, -1, 7, -1);
-// Instrucao* instrucao5 = new Instrucao(TIPO_R, 8, 10, -1, -1, FUNCAO_MULT);
-// Instrucao* instrucao6 = new Instrucao(SW, -1, -1, 24, 3, -1);
-// Instrucao* instrucao7 = new Instrucao(J, -1, -1, -1, 0, -1);
-// Instrucao* instrucao8 = new Instrucao(TIPO_R, 10, 8, 9, -1, FUNCAO_ADD);
-// Instrucao* instrucao9 = new Instrucao(SW, -1, -1, 9, 3, -1);
-// Instrucao* instrucao10 = new Instrucao(J, -1, -1, -1, 10, -1);
-
-// instrucoes->escrever(0, instrucao1);
-// instrucoes->escrever(1, instrucao2);
-// instrucoes->escrever(2, instrucao3);
-// instrucoes->escrever(3, instrucao4);
-// instrucoes->escrever(4, instrucao5);
-// instrucoes->escrever(5, instrucao6);
-// instrucoes->escrever(6, instrucao7);
-// instrucoes->escrever(7, instrucao8);
-// instrucoes->escrever(8, instrucao9);
-// instrucoes->escrever(9, instrucao10);
+int memoriaPosicao;
+int memoriaValor;
+int registradoresPosicao;
+int registradoresValor;
+string arquivoOrigem;
+string arquivoDestino;
 
 while(escolha == -1) {
     cout << "Emulador de MIPS" << endl;
@@ -72,7 +52,6 @@ while(escolha == -1) {
     cin >> escolhido;
     cout << endl;
 
-
     while(escolhido == 1) {
         cout << "Memoria" << endl;
         cout << "1) Alterar dado" << endl;
@@ -84,47 +63,48 @@ while(escolha == -1) {
 
         if(escolhaMemoria == 1){
             cout << "Posicao a ser alterada: ";
-            cin >> dadoPosicao;
+            cin >> memoriaPosicao;
             cout << "Novo valor: ";
-            cin >> dadoValor;
+            cin >> memoriaValor;
             cout << endl;
 
-            dadoMain = new Dado(dadoValor);
-            dados->escrever(dadoPosicao, dadoMain);
+            dadoMain = new Dado(memoriaValor);
+            memoriaRam->escrever(memoriaPosicao, dadoMain);
         }
 
         if(escolhaMemoria == 2){
-            dados->imprimir();
+            memoriaRam->imprimir();
         }
 
         if (escolhaMemoria == 0){
             break;
         }
     }
+
     while(escolhido == 2) {
         cout << "Registradores" << endl;
         cout << "1) Alterar valor" << endl;
         cout << "2) Imprimir" << endl;
         cout << "0) Voltar" << endl;
         cout << "Escolha uma opcao: ";
-        cin >> escolhaRegistrador;
+        cin >> escolhaRegistradores;
         cout << endl;
 
-        if(escolhaRegistrador == 1){
+        if(escolhaRegistradores == 1){
             cout << "Posicao a ser alterada: ";
-            cin >> registradorPosicao;
+            cin >> registradoresPosicao;
             cout << "Novo valor: ";
-            cin >> registradorValor;
+            cin >> registradoresValor;
             cout << endl;
 
-            registradores->setValor(registradorPosicao, registradorValor);
+            registradores->setValor(registradoresPosicao, registradoresValor);
         }
 
-        if(escolhaRegistrador == 2){
+        if(escolhaRegistradores == 2){
             registradores->imprimir();
         }
         
-        if (escolhaRegistrador == 0) {
+        if (escolhaRegistradores == 0) {
             break;
         }
     }
@@ -137,10 +117,22 @@ while(escolha == -1) {
         cout << endl;
     }
 
+    if(escolhido == 4){
+        while(controle->getPC() != 0)
+            controle->executarInstrucao();
+    }
+    
+    if(escolhido == 5){
+        cout << "Arquivo origem: " << arquivoOrigem;
+    }
+
+    if(escolhido == 6){
+        cout << "Arquivo destino: " << arquivoDestino;
+    }
+
     if(escolhido == 0){
         delete controle;
         return 0;
     }
-    
 }
 }
