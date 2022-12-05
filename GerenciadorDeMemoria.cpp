52f8a00ebbebee4f0b56c846c90810fd16010276
PeSartori <pkisartori@gmail.com>
2022-12-04 21:44:01 -0300

    cara

--- GerenciadorDeMemoria.cpp
+++ GerenciadorDeMemoria.cpp
@@ -6,164 +6,170 @@ GerenciadorDeMemoria::GerenciadorDeMemoria(){
 GerenciadorDeMemoria::~GerenciadorDeMemoria(){
 }
 
-void GerenciadorDeMemoria::load(string arquivo, MemoriaRAM* m){
+void GerenciadorDeMemoria::load(string arquivo, MemoriaRAM *m)
+{
     ifstream ler;
     ler.open(arquivo);
 
     if (ler.fail()){
-        throw new runtime_error ("runtime_error");
+        throw new runtime_error("runtime_error");
     }
 
-    int tamanho, valor, valor2, valor3, posicao = 0;
     string tipo;
-
-    list<Dado*>* listaDeDados;
-    Dado* dado;
+    int posicao = 0, tamanho;
+    int imediato, destino, origem1, origem2;
+    int valor;
 
     ler >> tamanho;
-    if(tamanho > m->getTamanho())
+    if (tamanho > m->getTamanho())
         throw new runtime_error("runtime_error");
 
-    ler >> tipo;
-    while(ler && posicao < m->getTamanho()){
-        if(tipo == "D"){
+    while (ler && posicao < m->getTamanho())
+    {
+        ler >> tipo;
+        if (tipo == "D"){
             ler >> valor;
-            dado = new Dado(valor);
-            listaDeDados->push_back(dado);
+            Dado *dado = new Dado(valor);
+            m->escrever(posicao++, dado);
+            continue;
         }
         else{
-            if(tipo == "LW"){
-                ler >> valor >> valor2;
-                listaDeDados->push_back(Instrucao::criarLW(valor, valor2));
+            if (tipo == "LW"){
+                ler >> destino;
+                ler >> imediato;
+                m->escrever(posicao++, Instrucao::criarLW(destino, imediato));
                 continue;
             }
-            if(tipo == "SW"){
-                ler >> valor >> valor2;
-                listaDeDados->push_back(Instrucao::criarSW(valor, valor2));
+            if (tipo == "SW"){
+                ler >> destino;
+                ler >> imediato;
+                m->escrever(posicao++, Instrucao::criarSW(destino, imediato));
                 continue;
             }
-            if(tipo == "J"){
-                ler >> valor;
-                listaDeDados->push_back(Instrucao::criarJ(valor));
+            if (tipo == "J"){
+                ler >> imediato;
+                m->escrever(posicao++, Instrucao::criarJ(imediato));
                 continue;
             }
-            if(tipo == "BNE"){
-                ler >> valor >> valor2 >> valor3;
-                listaDeDados->push_back(Instrucao::criarBNE(valor, valor2, valor3));
+            if (tipo == "BNE"){
+                ler >> origem1;
+                ler >> origem2;
+                ler >> imediato;
+                m->escrever(posicao++, Instrucao::criarBNE(origem1, origem2, imediato));
                 continue;
             }
-            if(tipo == "BEQ"){
-                ler >> valor >> valor2 >> valor3;
-                listaDeDados->push_back(Instrucao::criarBEQ(valor, valor2, valor3));
+            if (tipo == "BEQ"){
+                ler >> origem1;
+                ler >> origem2;
+                ler >> imediato;
+                m->escrever(posicao++, Instrucao::criarBEQ(origem1, origem2, imediato));
                 continue;
             }
-            if(tipo == "ADD"){
-                ler >> valor >> valor2 >> valor3;
-                listaDeDados->push_back(Instrucao::criarADD(valor, valor2, valor3));
+            if (tipo == "ADD"){
+                ler >> destino;
+                ler >> origem1;
+                ler >> origem2;
+                m->escrever(posicao++, Instrucao::criarADD(destino, origem1, origem2));
                 continue;
             }
-            if(tipo =="SUB"){
-                ler >> valor >> valor2 >> valor3;
-                listaDeDados->push_back(Instrucao::criarSUB(valor, valor2, valor3));
+            if (tipo == "SUB"){
+                ler >> destino;
+                ler >> origem1;
+                ler >> origem2;
+                m->escrever(posicao++, Instrucao::criarSUB(destino, origem1, origem2));
                 continue;
             }
-            if(tipo == "MULT"){
-                ler >> valor >> valor2;
-                listaDeDados->push_back(Instrucao::criarMULT(valor, valor2));
+            if (tipo == "MULT"){
+                ler >> origem1;
+                ler >> origem2;
+                m->escrever(posicao++, Instrucao::criarMULT(origem1, origem2));
                 continue;
             }
-            if(tipo == "DIV"){
-                ler >> valor >> valor2;
-                listaDeDados->push_back(Instrucao::criarDIV(valor, valor2)); 
-                continue;           
+            if (tipo == "DIV"){
+                ler >> origem1;
+                ler >> origem2;
+                m->escrever(posicao++, Instrucao::criarDIV(origem1, origem2));
+                continue;
             }
-            if(tipo == "-"){
-                listaDeDados->push_back(NULL);
+            if (tipo == "-"){
+                m->escrever(posicao++, NULL);
                 continue;
             }
         }
-        posicao++;
-        ler >> tipo;
     }
-
-    if(!ler.eof()){
-         throw new runtime_error ("runtime_error");
-         ler.close();
-    }
-
-    m->escrever(listaDeDados);
-
     ler.close();
 }
 
-void GerenciadorDeMemoria::dump(string arquivo, MemoriaRAM* m){
+void GerenciadorDeMemoria::dump(string arquivo, MemoriaRAM *m)
+{
     ofstream escrever;
     escrever.open (arquivo, ios_base::app);
 
-    escrever << m->getTamanho() << endl;
-    Instrucao *instrucao;
-    for(int i = 0; i < m->getTamanho(); i++){
-        if(m->ler(i) == NULL)
+    if (escrever.fail())
+        throw new runtime_error("runtime_error");
+
+    else
+        escrever << m->getTamanho() << endl;
+
+    for (int i = 0; i < m->getTamanho(); i++){
+
+        Instrucao *instrucao = dynamic_cast<Instrucao *>(m->ler(i));
+
+        if (m->ler(i) == NULL){
             escrever << "-" << endl;
-        
-        instrucao = dynamic_cast<Instrucao*>(m->ler(i));
+            continue;
+        }
+
         if(instrucao == NULL)
             escrever << "D " << m->ler(i)->getValor() << endl;
-        else{
-            if(instrucao->getOpcode() == Instrucao::LW){
-                escrever << "LW " << instrucao->getDestino() << " " << instrucao->getImediato() << endl;
-                continue;
-            }
-            if(instrucao->getOpcode() == Instrucao::SW){
-                escrever << "SW " << instrucao->getDestino() << " " << instrucao->getImediato() <<  endl;
-                continue;
-            }
 
-            if(instrucao->getOpcode() == Instrucao::J){
+        else{
+            if (instrucao->getOpcode() == Instrucao::J){
                 escrever << "J " << instrucao->getImediato() << endl;
                 continue;
             }
 
-            if(instrucao->getOpcode() == Instrucao::BNE){
+            if (instrucao->getOpcode() == Instrucao::BNE){
                 escrever << "BNE " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << " " << instrucao->getImediato() << endl;
                 continue;
             }
 
-            if(instrucao->getOpcode() == Instrucao::BEQ){
+            if (instrucao->getOpcode() == Instrucao::BEQ){
                 escrever << "BEQ " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << " " << instrucao->getImediato() << endl;
                 continue;
             }
 
-            if(instrucao->getOpcode() == Instrucao::TIPO_R){
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_ADD){
+            if (instrucao->getOpcode() == Instrucao::SW){
+                escrever << "SW " << instrucao->getDestino() << " " << instrucao->getImediato() << endl;
+                continue;
+            }
+
+            if (instrucao->getOpcode() == Instrucao::LW){
+                escrever << "LW " << instrucao->getDestino() << " " << instrucao->getImediato() << endl;
+                continue;
+            }
+
+            if (instrucao->getOpcode() == 0){
+                if (instrucao->getFuncao() == Instrucao::FUNCAO_ADD){
                     escrever << "ADD " << instrucao->getDestino() << " " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
                     continue;
                 }
-
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_SUB){
-                    escrever << "SUB " << instrucao->getDestino() << " " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
+                if (instrucao->getFuncao() == Instrucao::FUNCAO_SUB){
+                    escrever << "SUB " <<instrucao->getDestino() << " " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
                     continue;
                 }
 
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_MULT){
+                if (instrucao->getFuncao() == Instrucao::FUNCAO_MULT){
                     escrever << "MULT " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
                     continue;
                 }
 
-                if(instrucao->getFuncao() == Instrucao::FUNCAO_DIV){
-                   escrever << "DIV " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
-                   continue;
+                if (instrucao->getFuncao() == Instrucao::FUNCAO_DIV){
+                    escrever << "DIV " << instrucao->getOrigem1() << " " << instrucao->getOrigem2() << endl;
+                    continue;
                 }
             }
-
-            if(escrever.fail())
-                throw new runtime_error ("runtime_error");
-
-            int verificaOP = instrucao->getOpcode();
-
-            if((verificaOP != Instrucao::TIPO_R) && (verificaOP != Instrucao::LW) && (verificaOP != Instrucao::SW) && 
-            (verificaOP != Instrucao::BNE) && (verificaOP != Instrucao::BEQ) && (verificaOP != Instrucao::J) && (instrucao != NULL))
-                throw new runtime_error ("runtime_error");
         }
+        
     }
 }
